3529dab552164d738b07e9d82ffa9dcf85dff2bc
ChetanRamesh <chetanramesh@ChetanRameshs-MacBook-Pro.local>
2023-08-02 10:03:13 +0530

    adding volumes

--- k8s-cluster-automation/terraform/instances/main.tf
+++ k8s-cluster-automation/terraform/instances/main.tf
@@ -10,7 +10,7 @@ module "ec2_instance" {
   instance_type  = "t2.medium"
   key_name       = "nvirginia"
   subnet_ids     = ["subnet-03c1e3efc05a20333", "subnet-0a1698f89d640a509"]
-  instance_count = 2
+  instance_count = 3
 
   inbound_from_port  = ["0", "6443", "22", "30000"]
   inbound_to_port    = ["65000", "6443", "22", "32768"]
