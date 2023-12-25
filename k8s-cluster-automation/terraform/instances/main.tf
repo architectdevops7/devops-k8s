48b535b5f9cd6e1dbd2a97f1260e691edf239c99
ChetanRamesh <chetanramesh@ChetanRameshs-MacBook-Pro.local>
2023-12-25 18:37:06 +0530

    adding terraform files

--- k8s-cluster-automation/terraform/instances/main.tf
+++ k8s-cluster-automation/terraform/instances/main.tf
@@ -12,10 +12,10 @@ module "ec2_instance" {
   subnet_ids     = ["subnet-05fd53d4910b7083b"]
   instance_count = 2
 
-  inbound_from_port  = ["0", "6443", "22", "30000"]
-  inbound_to_port    = ["65000", "6443", "22", "32768"]
-  inbound_protocol   = ["TCP", "TCP", "TCP", "TCP"]
-  inbound_cidr       = ["0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0"]
+  inbound_from_port  = ["0"]
+  inbound_to_port    = ["0"]
+  inbound_protocol   = ["-1"]
+  inbound_cidr       = ["0.0.0.0/0"]
   outbound_from_port = ["0"]
   outbound_to_port   = ["0"]
   outbound_protocol  = ["-1"]
